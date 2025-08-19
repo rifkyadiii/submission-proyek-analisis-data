ef9c6f27354ac8e68ce6343ae6e9f5b67d3bc855
rifkyadiii <rifkyadikusumah@gmail.com>
2025-08-19 20:06:23 +0700

    update kode

---
+++ README.md
@@ -0,0 +1,17 @@
+# submission
+Tugas Akhir Visualiasasi DBS Coding Camp
+
+## Setup Environment - Anaconda
+- conda create --name main-ds python=3.9.12
+- conda activate main-ds
+- pip install -r requirements.txt
+
+## Setup Environment - Shell/Terminal
+- mkdir submission
+- cd submission
+- pipenv install
+- pipenv shell
+- pip install -r requirements.txt
+
+## Run steamlit app
+streamlit run dashboard.py
--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -12,8 +12,15 @@ st.set_page_config(
 
 @st.cache_data
 def load_data():
-    day_df = pd.read_csv("day.csv")
-    hour_df = pd.read_csv("hour.csv")
+    try:
+        hour_df = pd.read_csv('../data/hour.csv')
+        day_df = pd.read_csv('../data/day.csv')
+        print("Data berhasil dimuat dari ../data")
+    except FileNotFoundError:
+        print("../data tidak ditemukan, mencoba memuat /data")
+        # Jika file tidak ditemukan, mencoba memuat main_data.csv
+        hour_df = pd.read_csv('data/hour.csv')
+        day_df = pd.read_csv('data/day.csv')
     
     day_df['dteday'] = pd.to_datetime(day_df['dteday'])
     hour_df['dteday'] = pd.to_datetime(hour_df['dteday'])
